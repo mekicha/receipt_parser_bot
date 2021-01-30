7da0621fbd6346f2844ede7eb94841277d8fe644
Emeka Icha <emeka.icha@gmail.com>
2021-01-30 18:05:46 +0100

    add config file

---
+++ config.py
@@ -0,0 +1,5 @@
+MINDEE_API_TOKEN = "PUT_YOUR_TOKEN_HERE"
+BOT_TOKEN = "PUT_YOUR_TOKEN_HERE"
+MINDEE_URL = "https://api.mindee.net/products/expense_receipts/v2/predict"
+MINDEE_API_CALLS_COUNT = 50  # in a month
+LOG_LEVEL = "INFO"
