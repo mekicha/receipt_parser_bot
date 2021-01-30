39efc9c36e4a5e3d7effe51dd18fc408c25e9962
Emeka Icha <emeka.icha@gmail.com>
2021-01-30 17:41:16 +0100

    add type hint

--- handlers.py
+++ handlers.py
@@ -1,14 +1,18 @@
 import io
+from typing import Dict, Any
 from dateutil import parser
+
+import requests
 from peewee import fn
+from telegram import Update
+from telegram.ext.callbackcontext import CallbackContext
 
 import config
 from db import Expense
 
 
-def parse_receipt(image: io.BytesIO):
+def _parse_receipt(image: io.BytesIO) -> Dict[str, Any]:
     """ Sends the image to mindee api for parsing of receipt data """
-    import requests
 
     files = {"file": image.getvalue()}
     headers = {"X-Inferuser-Token": config.MINDEE_API_TOKEN}
@@ -19,21 +23,20 @@ def parse_receipt(image: io.BytesIO):
     # todo: fallback to Google vision api or setup mechanism to retry later
 
 
-def start_handler(update, context):
+def start_handler(update: Update, context: CallbackContext):
     context.bot.send_message(
         chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
     )
 
 
-def image_handler(update, context):
+def image_handler(update: Update, context: CallbackContext):
     # the last image in the list is the original size sent
     # use the first image if you want a smaller size
     image = [photo.get_file() for photo in update.message.photo][-1]
 
-    # download the image
-
     im = io.BytesIO(image.download_as_bytearray())
-    res = parse_receipt(im)
+    res = _parse_receipt(im)
+
     prediction = res["predictions"][0]
     date = prediction["date"]["raw"]
     date = parser.parse(date).date()
@@ -47,14 +50,14 @@ def image_handler(update, context):
     )
 
 
-def unknown_handler(update, context):
+def unknown_handler(update: Update, context: CallbackContext):
     context.bot.send_message(
         chat_id=update.effective_chat.id,
         text="Uhm not sure I got that, sorry :( Try typing /help",
     )
 
 
-def total_handler(update, context):
+def total_handler(update: Update, context: CallbackContext):
     """ Return the total spent so far """
     total = Expense.select(fn.SUM(Expense.amount)).scalar()
     context.bot.send_message(
@@ -62,7 +65,7 @@ def total_handler(update, context):
     )
 
 
-def report_handler(update, context):
+def report_handler(update: Update, context: CallbackContext):
     """ Get full report of expenses """
 
     Expense.select(fn.SUM(Expense.amount)).scalar()
